347dff2fa19b87730bafa2230db231bd1f3a6bab
pedrocasagrande98 <phccasagrande20@gmail.com>
2026-07-02 22:09:50 -0300

    correções de testes e configurações do projeto

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
  "django.contrib.messages",
  "django.contrib.staticfiles",
  'rest_framework',
+ 'rest_framework.authtoken',
+ 'product',
+ 'order',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
